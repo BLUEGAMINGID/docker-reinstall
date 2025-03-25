3d663bfe4810de19e246ac2e7c681cba4696092b
Danz <68720625+BLUEGAMINGID@users.noreply.github.com>
2025-03-25 23:03:25 +0700

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Uninstall old versions of Docker
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
+# Update the package index
+sudo apt-get update
+# Install packages to allow apt to use a repository over HTTPS
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent softwa>
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# Set up the stable repository
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+# Update the package index again
+sudo apt-get update
+# Install the latest version of Docker Engine and containerd
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+# Verify that Docker Engine is installed correctly by running the hello-world image
+sudo docker run hello-world
+# Enable Docker service to start on boot
+sudo systemctl enable docker
